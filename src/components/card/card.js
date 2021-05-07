1b47c0b1f2c53056c3272658446043d7564e90e8
trocoli96 <alfonso.trocoli@gmail.com>
2021-05-07 21:39:34 +0200

    test

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -4,7 +4,7 @@ import "./card.css";
 const Card = ({ monster }) => {
   return (
     <div className="_cardContainer" style={{ backgroundColor: monster.color }}>
-      <img src={monster.img} className="_card_image" />
+      <img src={monster.img} className="_card_image" alt={monster.name} />
     </div>
   );
 };
